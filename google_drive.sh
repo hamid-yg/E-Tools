31bc034b9692e56113b0ec9f6b67d86bd26ea181
Blacky <hamidou.tessilimi@epitech.eu>
2021-04-26 23:43:30 +0100

    update google drive script

--- google_drive.sh
+++ google_drive.sh
@@ -4,5 +4,6 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo -e "${GREEN}Mounting Google-drive account${NC}"
-google-drive-ocamlfuse GoogleDrive/hamidtessilimi@gmail.com/
-google-drive-ocamlfuse GoogleDrive/bde.epitechcotonou@gmail.com/
+
+google-drive-ocamlfuse -label hamid ~/GoogleDrive/hamidtessilimi@gmail.com
+google-drive-ocamlfuse -label bde ~/GoogleDrive/bde.epitechcotonou@gmail.com
